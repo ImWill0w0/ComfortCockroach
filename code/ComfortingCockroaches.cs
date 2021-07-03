0ba4aa3a8113a34187d414ab0e4f363138acae4c
ImWill0w0 <28235017+ImWill0w0@users.noreply.github.com>
2021-07-03 16:32:48 -0700

    update

--- code/ComfortingCockroaches.cs
+++ code/ComfortingCockroaches.cs
@@ -8,23 +8,12 @@ using System.Threading.Tasks;
 //
 // You don't need to put things in a namespace, but it doesn't hurt.
 //
-namespace MinimalExample
+namespace comfortcockroach
 {
-
-	/// <summary>
-	/// This is your game class. This is an entity that is created serverside when
-	/// the game starts, and is replicated to the client. 
-	/// 
-	/// You can use this to create things like HUDs and declare which player class
-	/// to use for spawned players.
-	/// 
-	/// Your game needs to be registered (using [Library] here) with the same name 
-	/// as your game addon. If it isn't then we won't be able to find it.
-	/// </summary>
 	[Library("comfortcockroach", Title = "comfortcockroach")]
-	public partial class ComfortingCockroaches : Sandbox.Game
+	public partial class comfortcockroach : Sandbox.Game
 	{
-		public ComfortingCockroaches()
+		public comfortcockroach()
 		{
 			if ( IsServer )
 			{
