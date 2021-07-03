e9d1d3edb39f565823f03c4b226c7dc2e881637e
ImWill0w0 <28235017+ImWill0w0@users.noreply.github.com>
2021-07-03 15:53:26 -0700

    update

--- code/ComfortingCockroaches.cs
+++ code/ComfortingCockroaches.cs
@@ -28,19 +28,12 @@ namespace MinimalExample
 		{
 			if ( IsServer )
 			{
-				Log.Info( "My Gamemode Has Created Serverside!" );
-
-				// Create a HUD entity. This entity is globally networked
-				// and when it is created clientside it creates the actual
-				// UI panels. You don't have to create your HUD via an entity,
-				// this just feels like a nice neat way to do it.
-				new MinimalHudEntity();
 				ConsoleSystem.Run("playsound cockroachmusic");
 			}
 
 			if ( IsClient )
 			{
-				Log.Info( "My Gamemode Has Created Clientside!" );
+				ConsoleSystem.Run("playsound cockroachmusic");
 			}
 		}
 
