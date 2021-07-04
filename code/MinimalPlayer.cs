aac8cf281229f9005a674f0ad0bf1c13317c8b51
ImWill0w0 <28235017+ImWill0w0@users.noreply.github.com>
2021-07-03 20:08:59 -0700

    update

--- code/MinimalPlayer.cs
+++ code/MinimalPlayer.cs
@@ -2,28 +2,28 @@
 using System;
 using System.Linq;
 
-namespace MinimalExample
+namespace comfortcockroach
 {
 	partial class MinimalPlayer : Player
 	{
 		public override void Respawn()
 		{
-			SetModel( "models/citizen/citizen.vmdl" );
+			//SetModel( "models/citizen/citizen.vmdl" );
 
 			//
 			// Use WalkController for movement (you can make your own PlayerController for 100% control)
 			//
-			Controller = new WalkController();
+			//Controller = new WalkController();
 
 			//
 			// Use StandardPlayerAnimator  (you can make your own PlayerAnimator for 100% control)
 			//
-			Animator = new StandardPlayerAnimator();
+			//Animator = new StandardPlayerAnimator();
 
 			//
 			// Use ThirdPersonCamera (you can make your own Camera for 100% control)
 			//
-			Camera = new ThirdPersonCamera();
+			Camera = new FirstPersonCamera();
 
 			EnableAllCollisions = true;
 			EnableDrawing = true;
@@ -31,34 +31,13 @@ namespace MinimalExample
 			EnableShadowInFirstPerson = true;
 
 			base.Respawn();
+
+			Camera = new StaticCamera();
 		}
 
 		/// <summary>
 		/// Called every tick, clientside and serverside.
 		/// </summary>
-		public override void Simulate( Client cl )
-		{
-			base.Simulate( cl );
-
-			//
-			// If you have active children (like a weapon etc) you should call this to 
-			// simulate those too.
-			//
-			SimulateActiveChild( cl, ActiveChild );
-
-			//
-			// If we're running serverside and Attack1 was just pressed, spawn a ragdoll
-			//
-			if ( IsServer && Input.Pressed( InputButton.Attack1 ) )
-			{
-				var ragdoll = new ModelEntity();
-				ragdoll.SetModel( "models/citizen/citizen.vmdl" );  
-				ragdoll.Position = EyePos + EyeRot.Forward * 40;
-				ragdoll.Rotation = Rotation.LookAt( Vector3.Random.Normal );
-				ragdoll.SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
-				ragdoll.PhysicsGroup.Velocity = EyeRot.Forward * 1000;
-			}
-		}
 
 		public override void OnKilled()
 		{
